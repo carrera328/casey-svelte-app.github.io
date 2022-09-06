cce23bac534e7891def422accb7ed284e3fd6055
dashcat <dashcat@pop-os.localdomain>
2022-09-06 00:41:12 -0400

    initial gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
+        repo: 'https://github.com/carrera328/casey-svelte-app', // Update to point to your repository  
         user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
+            name: 'Sal Carrera', // update to use your name
+            email: 'carrera.328@gmail.com' // Update to use your email
         }
     },
     () => {
