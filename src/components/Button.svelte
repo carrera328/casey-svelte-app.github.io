3d3813992e31ba6269debf12891206c4cc3f952e
Sal Carrera <=>
2022-10-11 00:08:36 -0400

    update

---
+++ src/components/Button.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let label;
+  export let name;
+
+  const dispatch = createEventDispatcher();
+  let element;
+  const handleClick = (event) => {
+    console.log('el', element.dataset.id);
+    
+
+    element.dispatchEvent(new CustomEvent('quoteclick', {
+      detail: element.dataset.id,
+      bubbles: true,
+      composed: true
+    }));
+    
+  };
+  
+
+
+</script>
+
+<button bind:this={element} on:click={handleClick} data-id={name}>{label}</button>
+
+<style>
+  button {
+    height: 100%;
+    background-color: #535E4B;
+    color: white;
+  }
+</style>
---
+++ src/components/Landing.svelte
@@ -0,0 +1,27 @@
+<script>
+    // TODO: Do INIT here
+    import Button from "./Button.svelte";
+    
+</script>
+
+
+<main>
+   <Button label='Funny' name='funny'></Button>
+   <Button label='Love' name='love'></Button>
+   <Button label='Surprise Cboo' name='random'></Button>
+   <Button label='Fun Facts' name='fun_facts'></Button>
+    
+</main>
+
+<style>
+    main {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        background-color: red;
+        width: 100%;
+        position: absolute;
+        bottom: 30%;
+        row-gap: 0;
+        column-gap: 0;
+    }
+</style>
